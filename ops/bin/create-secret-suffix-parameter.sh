649c5884f9f34d10a86288359bb2fb1b293e43fd
therealvio <41095688+therealvio@users.noreply.github.com>
2022-03-28 22:32:38 +1100

    Adds Infrastructure Stack for ralphbot (#11)
    * Adds prettier, eslint, and rimraf
    
    - `prettier` provides styling. This can be leveraged in CI to enforce
    code styling for the CDK Stack and its accompanying files.
    https://www.npmjs.com/package/prettier
    
    - `eslint` provides linting, fixing up problems. This can integrate with
    prettier as well.
    https://www.npmjs.com/package/eslint
    
    - `rimraf` has been added to provide a means to os-agnostically
    cleanup files left behind by tsc during linting, or builds where
    `--clean` wasn't used. If there is a better way of doing this when
    running eslint; I don't know about it.
    https://www.npmjs.com/package/rimraf
    
    * Expands CDK Stack
    
    The boilerplate CDK stack source code has been modified (to put it
    lightly) to have the necessary constructs, and resources to deploy
    ralphbot to AWS.
    
    * Adds helper scripts for secrets and ssm params
    
    These scripts provide the means to publish AWS SSM Parameters, and AWS
    Secrets Manager Secrets to an AWS Account that ralphbot would be
    deployed to. In doing so, these secrets are not published in source
    code in plaintext, and are only retrieved internally via AWS
    CloudFormation, or other appropriate AWS Services.

---
+++ ops/bin/create-secret-suffix-parameter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Preventing Credential leak in shell history
+export HISTCONTROL=ignorespace
+
+#Set variables
+#Remember to set appropriate values where "xxx" is used
+ENVIRONMENT='xxx' 
+
+# the following commands are prefixed with whitespace
+ SECRET_ARN_SUFFIX='xxx'
+
+ BOT_TOKEN_SECRET=$(printf '{
+     "token": "%s",
+     }' "$BOT_TOKEN")
+
+TAGS=$(printf '[
+    {
+        "Key": "environment",
+        "Value": "%s"
+    },
+]' "$ENVIRONMENT") 
+
+#Create MongoDB Atlas offsite-backup API Keypair
+aws ssm put-parameter \
+--name "/ralphbot/token/arn-suffix" \
+--description "The ARN suffix for Ralphbot's Secrets Manager secret. This is due to CDK not \"knowing\" what the full ARN is for a Secrets Manager secret by design." \
+--value "$SECRET_ARN_SUFFIX" \
+--tags "$TAGS"
