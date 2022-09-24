d844312a0fbf6936db4418c9b904abd02167a2a7
therealvio <41095688+therealvio@users.noreply.github.com>
2022-09-24 20:05:37 +1000

    fix: King's fall guide description

--- src/internal/command/guidefetch/guidefetch.go
+++ src/internal/command/guidefetch/guidefetch.go
@@ -16,7 +16,7 @@ var (
 			Options: []*discordgo.ApplicationCommandOption{
 				{
 					Name:        "raid-kingsfall",
-					Description: "Vow of the Disciple Raid",
+					Description: "King's Fall Raid",
 					Type:        discordgo.ApplicationCommandOptionSubCommand,
 				},
 				{
