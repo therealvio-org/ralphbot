008a3e8c55d86ba3d38562fbfad282c0d8ed7a57
therealvio <41095688+therealvio@users.noreply.github.com>
2022-10-31 14:22:39 +1100

    feat: Adds link-dump command
    A new slash-command has been added where, if invoked, will send a direct
    message to the invoker. The message contains a series of links defined
    in the `links.json` file. These links are for popular Destiny 2 web
    apps.
    
    This command was suggested to so that if newcomers asked for links to
    tools and services we use, we can instruct them to use the command
    rather than "fish" for it.

---
+++ src/internal/command/linkdump/linkdump.go
@@ -0,0 +1,81 @@
+package linkdump
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type LinkDetails struct {
+	Name string
+	URL  string
+}
+
+type Links struct {
+	Links []LinkDetails `json:"Links"`
+}
+
+//go:embed links.json
+var linkFile []byte
+
+var (
+	Commands = []*discordgo.ApplicationCommand{
+		//https://discord.com/developers/docs/interactions/application-commands#slash-commands
+		{
+			Name:        "link-dump",
+			Description: "Sends a link dump via DM of helpful Destiny 2-related web apps",
+		},
+	}
+
+	links, _ = getLinks(linkFile)
+
+	CommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"link-dump": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			channel, err := s.UserChannelCreate(i.Member.User.ID)
+			if err != nil {
+				log.Printf("Failed to create DM channel for %v: %v", i.User.ID, err)
+			}
+
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Flags:   discordgo.MessageFlagsEphemeral,
+					Content: "Check your DMs for a message from me! :smirk:",
+				},
+			})
+			if err != nil {
+				log.Printf("Failed to respond to interaction: %v", err)
+			}
+
+			_, err = s.ChannelMessageSend(channel.ID, links)
+			if err != nil {
+				log.Printf("Failed to send message to DM channel: %v", err)
+			}
+		},
+	}
+)
+
+func getLinks(b []byte) (string, error) {
+
+	var linkStruct Links
+	err := json.Unmarshal(b, &linkStruct)
+	if err != nil {
+		log.Printf("Unable to Unmarshal : %v", err)
+		return "", err
+	}
+
+	var linkSlice []string
+	for _, l := range linkStruct.Links {
+		//For some reason, markdown for links doesn't work here
+		linkElem := fmt.Sprintf("%s - <%s>", l.Name, l.URL)
+		linkSlice = append(linkSlice, linkElem)
+	}
+	content := strings.Join(linkSlice, "\n")
+
+	return content, nil
+
+}
