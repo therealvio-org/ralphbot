dde84e9a14029fe4153ecc412dd750b8455142f3
therealvio <41095688+therealvio@users.noreply.github.com>
2023-03-31 13:24:37 +1100

    test: `linkdump` and `guide-fetch` now use table-driven tests (#394)

--- src/internal/command/linkdump/linkdump_test.go
+++ src/internal/command/linkdump/linkdump_test.go
@@ -12,22 +12,44 @@ func TestGetLinks(t *testing.T) {
 	foo := "foo - <foo.com>"
 	bar := "bar - <bar.com>"
 
-	linkJson := `{
-		"Links": [
-			{
-			  "Name": "foo",
-			  "URL": "foo.com"
-			},
-			{
-			  "Name": "bar",
-			  "URL": "bar.com"
-			}
-		]
-	}`
+	cases := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name: "when one link is provided as input, the string contains one link",
+			input: `{
+				"Links": [
+					{
+					  "Name": "foo",
+					  "URL": "foo.com"
+					}
+				]
+			}`,
+			expected: foo,
+		},
+		{
+			name: "when 2 links are provided as input, the string contains two links, seperated by newline",
+			input: `{
+				"Links": [
+					{
+					  "Name": "foo",
+					  "URL": "foo.com"
+					},
+					{
+					  "Name": "bar",
+					  "URL": "bar.com"
+					}
+				]
+			}`,
+			expected: fmt.Sprintf("%s\n%s", foo, bar),
+		},
+	}
 
-	expected := fmt.Sprintf("%s\n%s", foo, bar)
-	links, err := getLinks([]byte(linkJson))
-
-	assert.NoError(t, err)
-	assert.Equal(t, expected, links)
+	for _, test := range cases {
+		result, err := getLinks([]byte(test.input))
+		assert.NoError(t, err)
+		assert.Equal(t, test.expected, result)
+	}
 }
